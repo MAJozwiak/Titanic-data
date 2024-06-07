4c75d82f823b8a7e35d6184abc5781763d0a2b72
Marta Jóźwiak <martajjoo9@gmail.com>
2024-06-07 14:05:40 +0200

    Import and read test data

--- src/read_clean_data/read_data.py
+++ src/read_clean_data/read_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 def import_data():
-    data=pd.read_csv(r"C:\Users\marta\PycharmProjects\Titanic-data\data\Titanic-Dataset.csv")
-    print(data)
-    return data
+    data_train=pd.read_csv(r"C:\Users\marta\PycharmProjects\Titanic-data\data\Titanic-Dataset.csv")
+    data_test=pd.read_csv(r"C:\Users\marta\PycharmProjects\Titanic-data\data\test.csv")
+    print(data_train)
+    print(data_test)
