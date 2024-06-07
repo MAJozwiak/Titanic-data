163219e7fd9f9b7b9fd2ea866636ce0f8af16253
Marta Jóźwiak <martajjoo9@gmail.com>
2024-06-08 01:21:05 +0200

    Add model training

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 from src.import_clean_data import read_data, clean_data
+from src.train_model import training
 
-#data import
-data_train,data_test=read_data.import_data()
-clean_data.clean_data(data_train)
-clean_data.clean_test_data(data_test)
+data_train, data_test = read_data.import_data()
+train=clean_data.clean_data(data_train)
+test=clean_data.clean_test_data(data_test)
+rf = training.evaluating(train)
+pred=training.prediciton(test, rf)
+training.accuracy(test,pred)
---
+++ src/train_model/training.py
@@ -0,0 +1,24 @@
+import graphviz
+import pandas as pd
+from PIL._imaging import display
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.tree import export_graphviz
+
+def evaluating(data):
+    rf = RandomForestClassifier()
+    X = data.drop('Survived', axis=1)
+    Y = data['Survived']
+    rf.fit(X,Y)
+    return rf
+def prediciton(data,rf):
+    X_test=data.drop('Survived', axis=1)
+    y_pred = rf.predict(X_test)
+    return y_pred
+def accuracy(data, y_pred):
+    y_test = data['Survived']
+    accuracy = accuracy_score(y_test, y_pred)
+    print("Accuracy:", accuracy)
+
+
+
