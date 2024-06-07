121a9211df3f5a52e6e154da6670bf9b4ac0d0b8
Marta Jóźwiak <martajjoo9@gmail.com>
2024-06-07 23:13:53 +0200

    Clean test data

--- src/main.py
+++ src/main.py
@@ -3,4 +3,4 @@ from src.import_clean_data import read_data, clean_data
 #data import
 data_train,data_test=read_data.import_data()
 clean_data.clean_data(data_train)
-
+clean_data.clean_test_data(data_test)
