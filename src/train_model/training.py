9263fb7abd83336a6a17ac33f9323d98ffc53122
Marta Jóźwiak <martajjoo9@gmail.com>
2024-06-10 20:37:39 +0200

    Apply GridSearchCV

--- src/train_model/training.py
+++ src/train_model/training.py
@@ -1,15 +1,18 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import GridSearchCV
 
 def evaluating(data):
     rf = RandomForestClassifier()
+    parameters = {'n_estimators': [100], 'max_depth': [2]}
+    clf = GridSearchCV(rf, parameters, cv=5)
     X = data.drop('Survived', axis=1)
     Y = data['Survived']
-    rf.fit(X,Y)
-    return rf
-def prediciton(data,rf):
+    clf.fit(X, Y)
+    return clf
+def prediciton(data,clf):
     X_test=data.drop('Survived', axis=1)
-    y_pred = rf.predict(X_test)
+    y_pred = clf.predict(X_test)
     return y_pred
 def accuracy(data, y_pred):
     y_test = data['Survived']
