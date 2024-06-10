42e6e7b809ed9251acdff314dbe3dd7da951eea1
Marta Jóźwiak <martajjoo9@gmail.com>
2024-06-10 18:48:38 +0200

    Type hinting for import_data function

--- src/import_clean_data/read_data.py
+++ src/import_clean_data/read_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
-def import_data(train_path, test_path):
+from pandas import DataFrame
+from typing import Tuple
+def import_data(train_path: str, test_path: str) -> Tuple[DataFrame, DataFrame]:
     data_train = pd.read_csv(train_path)
     data_test = pd.read_csv(test_path)
     return data_train, data_test
-
